f349111b4649bdafe3ef8e361e3109d63346d975
albert <albertxerta93@hotmail.com>
2014-11-05 19:35:11 +0100

    login ace

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -23,8 +23,8 @@ if (defined('ENVIRONMENT') && ENVIRONMENT=="development") {
       
       <!-- FAVICON-->    
 	  <link rel="shortcut icon" href="assets/icon/favicon.png">
-	  
-   <!-- CSS PROPIS -->
+	  <!--
+   <!-- CSS PROPIS --
 
        <link type="text/css" rel="stylesheet" href="<?php echo base_url('assets/skeleton/css/bootstrap-select.min.css')?>"/>
 
@@ -32,13 +32,22 @@ if (defined('ENVIRONMENT') && ENVIRONMENT=="development") {
        <link type="text/css" rel="stylesheet" href="<?php echo $bootstrap_combined_url;?>" /> 
    
 
-   <!-- JS PROPIS -->
+   <!-- JS PROPIS --
        <script src="<?php echo $jquery_minified_url;?>"></script>
        <script src="<?php echo $bootstrap_minified_url;?>"></script>
        <script src="<?php echo base_url('assets/skeleton/js/bootstrap-select.min.js')?>"></script>
-   
+   -->
+    <link href="<?php echo base_url('assets/css/bootstrap.min.css')?>" rel="stylesheet" />
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/font-awesome.min.css')?>" />
+
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/ace-fonts.css')?>" />
+
+    <!-- ace styles -->
+
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/ace.min.css')?>" />
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/ace-rtl.min.css')?>" />
    <style type="text/css">
-      body {
+      /*body {
         padding-top: 40px;
         padding-bottom: 40px;
         background-color: #f5f5f5;
@@ -67,22 +76,250 @@ if (defined('ENVIRONMENT') && ENVIRONMENT=="development") {
         height: auto;
         margin-bottom: 15px;
         padding: 7px 9px;
-      }
+      }*/
 
    </style>
    
-   <script type="text/javascript" id="bootstrap_select">
+   <!--<script type="text/javascript" id="bootstrap_select">
         $(window).on('load', function () {
             $('.selectpicker').selectpicker();
 		});
-   </script>
+   </script>-->
 
 
   </head>
 
-<body>
-	
-<div class="container">	
+
+<body class="login-layout">
+    <div class="main-container">
+      <div class="main-content">
+        <div class="row">
+          <div class="col-sm-10 col-sm-offset-1">
+            <div class="login-container">
+              <div class="center">
+                <h1>
+                  <i class="icon-leaf green"></i>
+                  <span class="red">Ace</span>
+                  <span class="white">Application</span>
+                </h1>
+                <h4 class="blue">&copy; Company Name</h4>
+              </div>
+
+              <div class="space-6"></div>
+
+              <div class="position-relative">
+                <div id="login-box" class="login-box visible widget-box no-border">
+                  <div class="widget-body">
+                    <div class="widget-main">
+                      <h4 class="header blue lighter bigger">
+                        <i class="icon-coffee green"></i>
+                        Please Enter Your Information
+                      </h4>
+
+                      <div class="space-6"></div>
+                      <?php echo form_open($login_url .$redirect, array('id' => 'loginform', 'class' => 'form-signin' )); ?>
+                        <fieldset>
+                          <label class="block clearfix">
+                            <span class="block input-icon input-icon-right">
+                              <input type="text" class="form-control" placeholder="<?php echo lang('User');?>" name="identity" />
+                              <i class="icon-user"></i>
+                            </span>
+                          </label>
+
+                          <label class="block clearfix">
+                            <span class="block input-icon input-icon-right">
+                              <input type="password" class="form-control" placeholder="<?php echo lang('Password');?>" name="password" />
+                              <i class="icon-lock"></i>
+                            </span>
+                          </label>
+                          <select id="realms" class="form-control" name="realm">
+                            <?php foreach( (array) $realms as $realm): ?>
+                              <?php if( $realm == $default_realm): ?>
+                                <option value="<?php echo $realm; ?>" selected="selected"><?php echo $realm; ?></option>
+                              <?php else: ?>
+                                <option value="<?php echo $realm; ?>" ><?php echo $realm; ?></option>
+                              <?php endif; ?>
+                            <?php endforeach; ?>
+                          </select>
+                          <div class="space"></div>
+
+                          <div class="clearfix">
+                            <label class="inline">
+                              <input type="checkbox" class="ace" />
+                              <span class="lbl"> Remember Me</span>
+                            </label>
+
+                            <button type="submit" class="width-35 pull-right btn btn-sm btn-primary">
+                              <i class="icon-key"></i>
+                              <?php echo lang('Login');?>
+                            </button>
+                          </div>
+
+                          <div class="space-4"></div>
+                        </fieldset>
+                      <?php echo form_close(); ?>
+
+                      <div class="social-or-login center">
+                        <span class="bigger-110">Or Login Using</span>
+                      </div>
+
+                      <div class="social-login center">
+                        <a class="btn btn-primary">
+                          <i class="icon-facebook"></i>
+                        </a>
+
+                        <a class="btn btn-info">
+                          <i class="icon-twitter"></i>
+                        </a>
+
+                        <a class="btn btn-danger">
+                          <i class="icon-google-plus"></i>
+                        </a>
+                      </div>
+                    </div><!-- /widget-main -->
+
+                    <div class="toolbar clearfix">
+                      <div>
+                        <a href="#" onclick="show_box('forgot-box'); return false;" class="forgot-password-link">
+                          <i class="icon-arrow-left"></i>
+                          I forgot my password
+                        </a>
+                      </div>
+
+                      <div>
+                        <a href="#" onclick="show_box('signup-box'); return false;" class="user-signup-link">
+                          I want to register
+                          <i class="icon-arrow-right"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </div><!-- /widget-body -->
+                </div><!-- /login-box -->
+
+                <div id="forgot-box" class="forgot-box widget-box no-border">
+                  <div class="widget-body">
+                    <div class="widget-main">
+                      <h4 class="header red lighter bigger">
+                        <i class="icon-key"></i>
+                        Retrieve Password
+                      </h4>
+
+                      <div class="space-6"></div>
+                      <p>
+                        Enter your email and to receive instructions
+                      </p>
+
+                      <form>
+                        <fieldset>
+                          <label class="block clearfix">
+                            <span class="block input-icon input-icon-right">
+                              <input type="email" class="form-control" placeholder="Email" />
+                              <i class="icon-envelope"></i>
+                            </span>
+                          </label>
+
+                          <div class="clearfix">
+                            <button type="button" class="width-35 pull-right btn btn-sm btn-danger">
+                              <i class="icon-lightbulb"></i>
+                              Send Me!
+                            </button>
+                          </div>
+                        </fieldset>
+                      </form>
+                    </div><!-- /widget-main -->
+
+                    <div class="toolbar center">
+                      <a href="#" onclick="show_box('login-box'); return false;" class="back-to-login-link">
+                        Back to login
+                        <i class="icon-arrow-right"></i>
+                      </a>
+                    </div>
+                  </div><!-- /widget-body -->
+                </div><!-- /forgot-box -->
+
+                <div id="signup-box" class="signup-box widget-box no-border">
+                  <div class="widget-body">
+                    <div class="widget-main">
+                      <h4 class="header green lighter bigger">
+                        <i class="icon-group blue"></i>
+                        New User Registration
+                      </h4>
+
+                      <div class="space-6"></div>
+                      <p> Enter your details to begin: </p>
+
+                      <form>
+                        <fieldset>
+                          <label class="block clearfix">
+                            <span class="block input-icon input-icon-right">
+                              <input type="email" class="form-control" placeholder="Email" />
+                              <i class="icon-envelope"></i>
+                            </span>
+                          </label>
+
+                          <label class="block clearfix">
+                            <span class="block input-icon input-icon-right">
+                              <input type="text" class="form-control" placeholder="Username" />
+                              <i class="icon-user"></i>
+                            </span>
+                          </label>
+
+                          <label class="block clearfix">
+                            <span class="block input-icon input-icon-right">
+                              <input type="password" class="form-control" placeholder="Password" />
+                              <i class="icon-lock"></i>
+                            </span>
+                          </label>
+
+                          <label class="block clearfix">
+                            <span class="block input-icon input-icon-right">
+                              <input type="password" class="form-control" placeholder="Repeat password" />
+                              <i class="icon-retweet"></i>
+                            </span>
+                          </label>
+
+                          <label class="block">
+                            <input type="checkbox" class="ace" />
+                            <span class="lbl">
+                              I accept the
+                              <a href="#">User Agreement</a>
+                            </span>
+                          </label>
+
+                          <div class="space-24"></div>
+
+                          <div class="clearfix">
+                            <button type="reset" class="width-30 pull-left btn btn-sm">
+                              <i class="icon-refresh"></i>
+                              Reset
+                            </button>
+
+                            <button type="button" class="width-65 pull-right btn btn-sm btn-success">
+                              Register
+                              <i class="icon-arrow-right icon-on-right"></i>
+                            </button>
+                          </div>
+                        </fieldset>
+                      </form>
+                    </div>
+
+                    <div class="toolbar center">
+                      <a href="#" onclick="show_box('login-box'); return false;" class="back-to-login-link">
+                        <i class="icon-arrow-left"></i>
+                        Back to login
+                      </a>
+                    </div>
+                  </div><!-- /widget-body -->
+                </div><!-- /signup-box -->
+              </div><!-- /position-relative -->
+            </div>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div>
+    </div><!-- /.main-container -->
+<!--<body>-->
+
+<!--<div class="container">	
 
      <center><div id="maintenance-mode-message" class="text-error"></div></center>
      
@@ -119,26 +356,33 @@ if (defined('ENVIRONMENT') && ENVIRONMENT=="development") {
           <?php endforeach; ?>	
          </select>
          
-         <!--
-         <br/>
-         
-         <label class="checkbox">
-           <input type="checkbox" value="remember-me"> <?php echo lang('remember');?> 
-         </label>
-         -->
 
         <br/>
         <button class="btn btn-large btn-primary" type="submit"><?php echo lang('Login');?></button>
        
        <?php echo form_close(); ?>
-       <!--<center><p><a href="<?php echo $register_url;?>"><?php echo lang('Register');?></a></p></center>-->
        <center><p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p></center>
        <br/>
        
-    	   <?php include("auth_footer.html"); ?>      
-       
-</div>
+    	   <?php include("auth_footer.html"); ?>
 
+</div>-->
+    <script type="text/javascript">
+      window.jQuery || document.write("<script src='assets/js/jquery-2.0.3.min.js'>"+"<"+"/script>");
+    </script>
+
+    <script type="text/javascript">
+      if("ontouchend" in document) document.write("<script src='assets/js/jquery.mobile.custom.min.js'>"+"<"+"/script>");
+    </script>
+
+    <!-- inline scripts related to this page -->
+
+    <script type="text/javascript">
+      function show_box(id) {
+       jQuery('.widget-box.visible').removeClass('visible');
+       jQuery('#'+id).addClass('visible');
+      }
+    </script>
 </body>
 
 </html>
